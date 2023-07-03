36059dd758e2c64edcd72ce716390310f2bead42
Samuel Yusa <Samuelyusa@gmail.com>
2023-07-03 22:34:43 +0700

    e2e-3

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,3 +1,6 @@
+const assert = require('assert');
+const { async } = require('regenerator-runtime');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
@@ -9,13 +12,16 @@ Scenario('showing empty liked restaurants', ({ I }) => {
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
 });
 
-Scenario('liking one restaurant', ({I}) => {
+Scenario('liking one restaurant', async ({I}) => {
   I.see('Restaurant tidak ditemukan', '.restaurant-item__not__found');
 
   I.amOnPage('/');
 
   I.waitForElement('.restaurant-item', 5);
   I.seeElement('.restaurant-item');
+
+  const firstRestaurant = locate('.restaurant__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
   I.waitForClickable('.restaurant__title a',6);
   I.click(locate('.restaurant__title a').first());
 
@@ -25,5 +31,8 @@ Scenario('liking one restaurant', ({I}) => {
 
   I.amOnPage('/#/favorite');
   I.seeElement('.restaurant-item');
+
+  const likedRestaurantTitle = await I.grabTextFrom('.restaurant__title');
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 
 });
